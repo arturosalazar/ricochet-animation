e0205c28dd67958f1793958992796eea1817252b
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2019-03-09 16:32:34 -0600

    make program modular by adding functions
    separate the program into parts by using self defined functions and calling those functions

--- sketch.js
+++ sketch.js
@@ -1,16 +1,16 @@
 /*
   Goal - make a ball bounce that bounces off any side using if statements
   Allow the ball to move along the x and y planes
-  Use || statements to condense the number of if statements from previous iteration
+
+  Use self defined functions to make a modular program
+  Define sections of code into functions and then call those functions
 */
 
-//x and y of the circle
 let circleLoc = {
   x: 0,
   y: 200
 }
 
-//speed at which the circle moves through x and y
 let speed = {
   x: 3,
   y: -3
@@ -27,34 +27,36 @@ function setup() {
   createCanvas(600, 400);
 }
 
-
 function draw() {
   background(0);
-  noFill();
+	displayCircle();
+  bounce();
+  move();
+}
+
+function displayCircle(){
+	noFill();
   stroke(colors.r, colors.g, colors.b, colors.a);
   strokeWeight(40);
   ellipse(circleLoc.x, circleLoc.y, 100, 100);
+}
 
-  //if statements that let the circles bounce off each side
-
-  //bounce top or bottom
+function bounce(){
   if (circleLoc.y < 0 || circleLoc.y > height) {
     speed.y = -speed.y;
     colors.r = random(100, 255);
     colors.b = random(100, 255);
   } 
-
-  //bounce right or left
   if (circleLoc.x > width || circleLoc.x <0) { 
     speed.x = -speed.x;
     colors.g = random(100, 255);
     colors.a = random(100, 255);
   }
+}
 
-  //animation for moving circle
+function move(){
   circleLoc.x = circleLoc.x + speed.x
   circleLoc.y = circleLoc.y + speed.y
-
 }
 
 function mousePressed() {
