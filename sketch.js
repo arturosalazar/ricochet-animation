b69a7c2594fc62aab9a9f03f9b8064c987ddff6a
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2019-03-09 16:30:05 -0600

    modify to use || statements

--- sketch.js
+++ sketch.js
@@ -1,30 +1,63 @@
 /*
-  Exercise for Coding Train 3.2
-  Goal - make a ball bounce that bounces off each side using if statements
+  Goal - make a ball bounce that bounces off any side using if statements
+  Allow the ball to move along the x and y planes
+  Use || statements to condense the number of if statements from previous iteration
 */
 
-let x = 0;
-let y = 200;
+//x and y of the circle
+let circleLoc = {
+  x: 0,
+  y: 200
+}
 
-let speedX = 3; 
+//speed at which the circle moves through x and y
+let speed = {
+  x: 3,
+  y: -3
+}
+
+let colors = {
+  r: 255,
+  g: 255,
+  b: 255,
+  a: 100,
+}
 
 function setup() {
   createCanvas(600, 400);
 }
 
+
 function draw() {
   background(0);
   noFill();
-  stroke(255);
-  strokeWeight(4);
-  ellipse (x,y,100,100);
+  stroke(colors.r, colors.g, colors.b, colors.a);
+  strokeWeight(40);
+  ellipse(circleLoc.x, circleLoc.y, 100, 100);
 
-  //change the direction of change in x whenever hitting one or other edge
-  if (x>width) {
-  	speedX = -speedX;
+  //if statements that let the circles bounce off each side
+
+  //bounce top or bottom
+  if (circleLoc.y < 0 || circleLoc.y > height) {
+    speed.y = -speed.y;
+    colors.r = random(100, 255);
+    colors.b = random(100, 255);
+  } 
+
+  //bounce right or left
+  if (circleLoc.x > width || circleLoc.x <0) { 
+    speed.x = -speed.x;
+    colors.g = random(100, 255);
+    colors.a = random(100, 255);
   }
-  if (x<0) {
-  	speedX = -speedX;  
-  }
-  x = x + speedX;  
+
+  //animation for moving circle
+  circleLoc.x = circleLoc.x + speed.x
+  circleLoc.y = circleLoc.y + speed.y
+
+}
+
+function mousePressed() {
+  speed.x = random(0, 8);
+  speed.y = random(0, 8);
 }
