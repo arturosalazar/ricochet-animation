3a7cc51a63928f3cd2bd207d4489c446e89f60d9
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2019-03-09 16:35:59 -0600

    Use objects to contain the pertinent functions

--- sketch.js
+++ sketch.js
@@ -2,19 +2,43 @@
   Goal - make a ball bounce that bounces off any side using if statements
   Allow the ball to move along the x and y planes
 
-  Use self defined functions to make a modular program
-  Define sections of code into functions and then call those functions
+  Goal - use object to contain functions for a bouncing ball
+  i.e. create methods on the object
 */
 
-//separate objects for the location, speed and colors of the ball
 let circleLoc = {
   x: 0,
-  y: 200
-}
-
-let speed = {
-  x: 3,
-  y: -3
+  y: 200,
+  speedX: 3,
+  speedY: -3,
+  bounce: function() {
+    /*
+      Note: here, "this" refers to the object "circleloc"
+      
+      Whenever a method contains the keyword "this", it refers to the object
+      the method is located on 
+    */
+    if (this.y < 0 || this.y > height) {
+      this.speedY = -this.speedY;
+      colors.r = random(100, 255);
+      colors.b = random(100, 255);
+    }
+    if (this.x > width || this.x < 0) {
+      this.speedX = -this.speedX;
+      colors.g = random(100, 255);
+      colors.a = random(100, 255);
+    }
+  },
+  move: function() {
+    this.x = this.x + this.speedX;
+    this.y = this.y + this.speedY;
+  },
+  displayCircle: function() {
+    noFill();
+    stroke(colors.r, colors.g, colors.b, colors.a);
+    strokeWeight(40);
+    ellipse(this.x, this.y, 100, 100);
+  }
 }
 
 let colors = {
@@ -30,37 +54,12 @@ function setup() {
 
 function draw() {
   background(0);
-  displayCircle();
-  bounce();
-  move();
-}
-
-function displayCircle(){
-  noFill();
-  stroke(colors.r, colors.g, colors.b, colors.a);
-  strokeWeight(40);
-  ellipse(circleLoc.x, circleLoc.y, 100, 100);
-}
-
-function bounce(){
-  if (circleLoc.y < 0 || circleLoc.y > height) {
-    speed.y = -speed.y;
-    colors.r = random(100, 255);
-    colors.b = random(100, 255);
-  } 
-  if (circleLoc.x > width || circleLoc.x <0) { 
-    speed.x = -speed.x;
-    colors.g = random(100, 255);
-    colors.a = random(100, 255);
-  }
-}
-
-function move(){
-  circleLoc.x = circleLoc.x + speed.x
-  circleLoc.y = circleLoc.y + speed.y
+  circleLoc.displayCircle();
+  circleLoc.bounce();
+  circleLoc.move();
 }
 
 function mousePressed() {
-  speed.x = random(0, 8);
-  speed.y = random(0, 8);
+  circleLoc.speedX = random(0, 8);
+  circleLoc.speedY = random(0, 8);
 }
