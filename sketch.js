54d93ac791f4ca0c508a49cfa8f649b12f44871d
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2019-03-09 17:04:35 -0600

    modified bounce to make it more realistic

--- sketch.js
+++ sketch.js
@@ -4,9 +4,10 @@
 
   Use object to contain functions for a bouncing ball
   i.e. create methods on the object
+  
+  Balls bounce off of each other more realistically
 */
-//Goal - with two balls bouncing, get them
-//to bounce off each other
+
 let circleLoc = {
   x: 0,
   y: 200,
@@ -18,19 +19,19 @@ let circleLoc = {
   a: 100,
   bounce: function() {
     if (this.y < 0 || this.y > height) {
-      this.speedY = -this.speedY;
+      this.speedY = this.speedY * -1;
       this.r = random(100, 255);
       this.b = random(100, 255);
     }
     if (this.x > width || this.x < 0) {
-      this.speedX = -this.speedX;
+      this.speedX = this.speedX * -1;
       this.g = random(100, 255);
       this.a = random(100, 255);
     }
   },
   move: function() {
-    this.x = this.x + this.speedX;
-    this.y = this.y + this.speedY;
+    this.x = this.x + this.speedX
+    this.y = this.y + this.speedY
   },
   displayCircle: function() {
     noFill();
@@ -62,8 +63,8 @@ let circleLoc2 = {
     }
   },
   move: function() {
-    this.x = this.x + this.speedX;
-    this.y = this.y + this.speedY;
+    this.x = this.x + this.speedX
+    this.y = this.y + this.speedY
   },
   displayCircle: function() {
     noFill();
@@ -73,17 +74,31 @@ let circleLoc2 = {
   }
 }
 
+//if the distance between the balls is < the distance between 
+//both centers when the balls touch, then bounce
 let ballInteraction = {
   distance:0,
   deflect: function() {
     this.distance = Math.sqrt((circleLoc.x - circleLoc2.x) * (circleLoc.x - circleLoc2.x) + (circleLoc.y - circleLoc2.y) * (circleLoc.y - circleLoc2.y))
     if (this.distance < 141) {
-      circleLoc.speedY = circleLoc.speedY * -1;
-      circleLoc.speedX = circleLoc.speedX * -1;
-      circleLoc2.speedY = circleLoc2.speedY * -1;
-      circleLoc2.speedX = circleLoc2.speedX * -1;
+      //cover the logic if balls are going in opposite directions (speedX of both balls are opposite pos/neg or speedY of both balls are opposite pos/neg)
+      //TODO: Circles sometimes get caught together and bounce back and forth "on top" of each other. Look on how to fix
+      if (circleLoc.speedX < 0 && circleLoc2.speedX > 0){
+      	circleLoc.speedX = circleLoc.speedX * -1;
+      	circleLoc2.speedX = circleLoc2.speedX * -1;
+      } else if (circleLoc.speedX > 0 && circleLoc2.speedX < 0){
+      	circleLoc.speedX = circleLoc.speedX * -1;
+      	circleLoc2.speedX = circleLoc2.speedX * -1;
+      }     
+      if (circleLoc.speedY < 0 && circleLoc2.speedY > 0){
+          circleLoc.speedY = circleLoc.speedY * -1;
+      		circleLoc2.speedY = circleLoc2.speedY * -1;
+      } else if (circleLoc.speedY > 0 && circleLoc2.speedY < 0){
+          circleLoc.speedY = circleLoc.speedY * -1;
+      		circleLoc2.speedY = circleLoc2.speedY * -1;
+      }
+      //TODO: Add functionality if they're both going in the same direction
     }
-
   }
 }
 
